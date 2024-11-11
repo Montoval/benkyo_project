b1cbf2330f5a03da92542f85525356ac2db57022
V1CT0R7890 <aserbalac7890@gmail.com>
2024-11-11 09:28:42 -0300

    pag

--- benkyo_project/calendario/index.php
+++ benkyo_project/calendario/index.php
@@ -182,7 +182,7 @@ $monthName = isset($months[$month]) ? $months[$month] : "Mês Inválido";
     <h1>Calendário de Eventos</h1>
     <h2><?= $monthName ?> de <?= $year ?></h2> <!-- Exibe o mês e o ano atuais em português -->
     <a class=pag href="?month=<?= $month == 1 ? 12 : $month - 1 ?>&year=<?= $month == 1 ? $year - 1 : $year ?>">Anterior</a>
-    <a id="prox" href="?month=<?= $month == 12 ? 1 : $month + 1 ?>&year=<?= $month == 12 ? $year + 1 : $year ?>">Próximo</a>
+    <a class=pag id="prox" href="?month=<?= $month == 12 ? 1 : $month + 1 ?>&year=<?= $month == 12 ? $year + 1 : $year ?>">Próximo</a>
     
     <table class="calendar">
         <tr>
