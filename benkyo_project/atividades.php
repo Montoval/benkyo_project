ff3c685fc49db7f8a8fa7c687d42c76ca6bd1c62
Joao0015 <joaovictor.simoesrosa@gmail.com>
2024-11-11 09:27:08 -0300

    insta

--- benkyo_project/atividades.php
+++ benkyo_project/atividades.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <title>Atividades</title>
     <link rel="stylesheet" href="styleP.css">
     <link rel="icon" type="image/x-icon" href="imagens/Benkyoicon2.jpg">
 </head>
+
 <body>
     <nav class="menu-fixo">
         <a href="principal.html" class="active">Inicio</a>
@@ -18,57 +20,43 @@
             <div class="box">
                 <div class="menuP">
 
-                  <div class="box-atividade">
-                   
-                    <?php
+                    <div class="box-atividade">
+
+                        <?php
                     include "atividade/atividade.php";
                     ?>
-                  </div>
-                  
-              <?php
+                    </div>
+
+                    <?php
            
               include "atividade/create.php";
       
               
               ?>
                 </div>
-                
+
                 <footer class="footer-fix">
                     <div class="waves">
-                      <div class="wave" id="wave1"></div>
-                      <div class="wave" id="wave2"></div>
-                      <div class="wave" id="wave3"></div>
-                      <div class="wave" id="wave4"></div>
+                        <div class="wave" id="wave1"></div>
+                        <div class="wave" id="wave2"></div>
+                        <div class="wave" id="wave3"></div>
+                        <div class="wave" id="wave4"></div>
                     </div>
                     <ul class="social-icon">
-                      <li class="social-icon__item"><a class="social-icon__link" href="#">
-                          <ion-icon name="logo-facebook"></ion-icon>
-                        </a></li>
-                      <li class="social-icon__item"><a class="social-icon__link" href="#">
-                          <ion-icon name="logo-twitter"></ion-icon>
-                        </a></li>
-                      <li class="social-icon__item"><a class="social-icon__link" href="#">
-                          <ion-icon name="logo-linkedin"></ion-icon>
-                        </a></li>
-                      <li class="social-icon__item"><a class="social-icon__link" href="#">
-                          <ion-icon name="logo-instagram"></ion-icon>
-                        </a></li>
+                        <li class="social-icon__item"><a class="social-icon__link" href="#">
+                                <ion-icon name="logo-instagram"></ion-icon>
+                            </a></li>
                     </ul>
                     <ul class="menu">
-                      <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-                      <li class="menu__item"><a class="menu__link" href="#">About</a></li>
-                      <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
-                      <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
-                      <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
-                
                     </ul>
-                  </footer>
-                  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-                  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+                </footer>
+                <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
             </div>
         </div>
-       
+
 
     </main>
 </body>
+
 </html>
--- benkyo_project/calendario/all_events.php
+++ benkyo_project/calendario/all_events.php
@@ -30,57 +30,64 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style_events.css">
     <link rel="icon" type="image/x-icon" href="../imagens/Benkyoicon2.jpg">
     <title>Todos os Eventos</title>
     <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        .delete-button {
-            background-color: #ff4d4d;
-            color: white;
-            border: none;
-            padding: 5px 10px;
-            cursor: pointer;
-            border-radius: 4px;
-        }
-        .delete-button:hover {
-            background-color: #ff1a1a;
-        }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th,
+    td {
+        border: 1px solid #ddd;
+        padding: 8px;
+        text-align: left;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    .delete-button {
+        background-color: #ff4d4d;
+        color: white;
+        border: none;
+        padding: 5px 10px;
+        cursor: pointer;
+        border-radius: 4px;
+    }
+
+    .delete-button:hover {
+        background-color: #ff1a1a;
+    }
     </style>
 </head>
+
 <body>
-    
-  <nav class="menu-fixo">
+
+    <nav class="menu-fixo">
         <a href="../calendario.php">Voltar</a>
-  </nav>
-  <main>
-    <div class="container">
-      <div class="box">
-          <div class="evento">
-            <h1>Todos os Eventos</h1>
-    </div>
-    <table>
-        <tr>
-            <th>Data</th>
-            <th>Local</th>
-            <th>Hora</th>
-            <th>Atividade</th>
-            <th>Ações</th>
-        </tr>
-        <?php 
+    </nav>
+    <main>
+        <div class="container">
+            <div class="box">
+                <div class="evento">
+                    <h1>Todos os Eventos</h1>
+                </div>
+                <table>
+                    <tr>
+                        <th>Data</th>
+                        <th>Local</th>
+                        <th>Hora</th>
+                        <th>Atividade</th>
+                        <th>Ações</th>
+                    </tr>
+                    <?php 
         foreach ($events as $event) {
             $data = htmlspecialchars($event['dataEvento']);
             $local = htmlspecialchars($event['localEvento']);
@@ -101,44 +108,29 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
                   </tr>";
         }
         ?>
-    </table>
-    <footer class="footer-fix">
-          <div class="waves">
-            <div class="wave" id="wave1"></div>
-            <div class="wave" id="wave2"></div>
-            <div class="wave" id="wave3"></div>
-            <div class="wave" id="wave4"></div>
-          </div>
-          <ul class="social-icon">
-            <li class="social-icon__item"><a class="social-icon__link" href="#">
-                <ion-icon name="logo-facebook"></ion-icon>
-              </a></li>
-            <li class="social-icon__item"><a class="social-icon__link" href="#">
-                <ion-icon name="logo-twitter"></ion-icon>
-              </a></li>
-            <li class="social-icon__item"><a class="social-icon__link" href="#">
-                <ion-icon name="logo-linkedin"></ion-icon>
-              </a></li>
-            <li class="social-icon__item"><a class="social-icon__link" href="#">
-                <ion-icon name="logo-instagram"></ion-icon>
-              </a></li>
-          </ul>
-          <ul class="menu">
-            <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">About</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
-            <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
-
-          </ul>
-        </footer>
-        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-      </div>
-    </div>
+                </table>
+                <footer class="footer-fix">
+                    <div class="waves">
+                        <div class="wave" id="wave1"></div>
+                        <div class="wave" id="wave2"></div>
+                        <div class="wave" id="wave3"></div>
+                        <div class="wave" id="wave4"></div>
+                    </div>
+                    <ul class="social-icon">
+                        <li class="social-icon__item"><a class="social-icon__link" href="#">
+                                <ion-icon name="logo-instagram"></ion-icon>
+                            </a></li>
+                    </ul>
+                    <ul class="menu">
+                    </ul>
+                </footer>
+                <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+            </div>
+        </div>
 
 
-  </main>
+    </main>
 </body>
 
 </html>
